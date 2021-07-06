b6d1cc7f60d2878efa31fe9f6bc056899422e632
Jesse C <76225346+Jesse2360@users.noreply.github.com>
2021-07-06 01:33:57 -0500

    add server.js file

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+});
+
+//Routes
+app.use(require('./Develop/routes/api-Routes.js'));
+app.use(require('./Develop/routes/html-Routes.js'));
+
+app.listen(PORT, () => {
+    console.log(`Workout Tracker app is now running on Port ${PORT}!`);
+});
